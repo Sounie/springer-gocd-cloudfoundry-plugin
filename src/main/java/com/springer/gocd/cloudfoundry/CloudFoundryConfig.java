2f29815d00d3505bf705d1146a54fafa2a9a6874
Stephen Souness <stephen.souness@springer.com>
2015-02-06 10:59:08 +0000

    First consideration of CloudFoundry credentials setup.

--- src/main/java/com/springer/gocd/cloudfoundry/CloudFoundryConfig.java
+++ src/main/java/com/springer/gocd/cloudfoundry/CloudFoundryConfig.java
@@ -7,6 +7,8 @@ public class CloudFoundryConfig {
     private final String username;
     private final String password;
     private final String space;
+    private final String appName;
+    private final String orgName;
 
 
     public CloudFoundryConfig(Map<String, String> config) {
@@ -14,7 +16,32 @@ public class CloudFoundryConfig {
         this.username = config.get("username");
         this.password = config.get("password");
         this.space = config.get("space");
+        this.appName = config.get("appName");
+        this.orgName = config.get("orgName");
     }
 
 
+    public String getApiUrl() {
+        return apiUrl;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getSpace() {
+        return space;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
 }
--- src/main/java/com/springer/gocd/cloudfoundry/CloudFoundryPlugin.java
+++ src/main/java/com/springer/gocd/cloudfoundry/CloudFoundryPlugin.java
@@ -3,13 +3,16 @@ package com.springer.gocd.cloudfoundry;
 import com.google.gson.GsonBuilder;
 import com.thoughtworks.go.plugin.api.AbstractGoPlugin;
 import com.thoughtworks.go.plugin.api.GoPluginIdentifier;
+import com.thoughtworks.go.plugin.api.annotation.Extension;
 import com.thoughtworks.go.plugin.api.exceptions.UnhandledRequestTypeException;
 import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 
+@Extension
 public class CloudFoundryPlugin extends AbstractGoPlugin {
     private final GoPluginIdentifier identifier;
 
@@ -23,11 +26,19 @@ public class CloudFoundryPlugin extends AbstractGoPlugin {
         GsonBuilder gsonBuilder = new GsonBuilder();
         Map executionRequest = (Map) gsonBuilder.create().fromJson(request.requestBody(), Object.class);
 
-        CloudFoundryRequestExecutor requestExecutor = new CloudFoundryRequestExecutor(
-                new CloudFoundryConfig(executionRequest)
-        );
+        if ("execute".equals(request.requestName())) {
+            CloudFoundryRequestExecutor requestExecutor = new CloudFoundryRequestExecutor(
+                    new CloudFoundryConfig(executionRequest)
+            );
 
-        return null;
+            Collection<String> versions =  requestExecutor.getVersions();
+
+            System.out.println("DEBUG - versions = " + versions);
+        }
+
+
+
+        throw new UnhandledRequestTypeException(request.requestName());
     }
 
     @Override
