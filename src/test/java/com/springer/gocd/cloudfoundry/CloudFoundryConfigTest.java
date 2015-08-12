53c271a0e84117d8809afed23ffabd3147727ec8
Stephen Souness <sounie@hotmail.com>
2015-08-12 23:01:51 +0100

    Tidied up imports.

--- src/test/java/com/springer/gocd/cloudfoundry/CloudFoundryConfigTest.java
+++ src/test/java/com/springer/gocd/cloudfoundry/CloudFoundryConfigTest.java
@@ -5,8 +5,6 @@ import com.thoughtworks.go.plugin.api.material.packagerepository.RepositoryConfi
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.*;
 
 public class CloudFoundryConfigTest {
